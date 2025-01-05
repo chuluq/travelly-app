21f916910a18f718a85310588bde18fb9b218289
chuluq <chuluq@arvis.id>
2025-01-05 15:44:17 +0700

    chore: setup zustand

--- src/app/(dashboard)/dashboard/articles/page.tsx
+++ src/app/(dashboard)/dashboard/articles/page.tsx
@@ -1,9 +1,21 @@
+import { Metadata } from "next";
+
+import { ArticleList } from "@/components/articles/list";
 import { API_URL, article } from "@/lib/constants";
+
+export const metadata: Metadata = {
+  title: "Article List",
+  description: "Lorem ipsum dolor sit amet.",
+};
 
 export default async function ArticlePage() {
   const data = await fetch(`${API_URL}/api/${article.list}`);
   const articles = await data.json();
   console.log("articles", articles);
 
-  return <div>ArticlePage</div>;
+  return (
+    <div>
+      <ArticleList />
+    </div>
+  );
 }
--- src/app/(dashboard)/dashboard/layout.tsx
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -3,6 +3,7 @@ import { redirect } from "next/navigation";
 import { getSession } from "@/actions/auth";
 import { MainNav } from "@/components/main-nav";
 import { dashboardConfig } from "@/config/dashboard";
+import { CounterStoreProvider } from "@/providers/articles-store-provider";
 
 interface DashboardLayoutProps {
   children?: React.ReactNode;
@@ -18,21 +19,23 @@ export default async function DashboardLayout({
   }
 
   return (
-    <div className="flex min-h-screen flex-col space-y-6">
-      <header className="sticky top-0 z-40 border-b bg-background">
-        <div className="container flex h-16 items-center justify-between py-4">
-          <MainNav items={dashboardConfig.mainNav} />
+    <CounterStoreProvider>
+      <div className="flex min-h-screen flex-col space-y-6">
+        <header className="sticky top-0 z-40 border-b bg-background">
+          <div className="container flex h-16 items-center justify-between py-4">
+            <MainNav items={dashboardConfig.mainNav} />
+          </div>
+        </header>
+        <div className="container grid flex-1 gap-12 md:grid-cols-[200px_1fr]">
+          <aside className="hidden w-[200px] flex-col md:flex">
+            dashboard nav
+          </aside>
+          <main className="flex w-full flex-1 flex-col overflow-hidden">
+            {children}
+          </main>
         </div>
-      </header>
-      <div className="container grid flex-1 gap-12 md:grid-cols-[200px_1fr]">
-        <aside className="hidden w-[200px] flex-col md:flex">
-          dashboard nav
-        </aside>
-        <main className="flex w-full flex-1 flex-col overflow-hidden">
-          {children}
-        </main>
+        site footer
       </div>
-      site footer
-    </div>
+    </CounterStoreProvider>
   );
 }
